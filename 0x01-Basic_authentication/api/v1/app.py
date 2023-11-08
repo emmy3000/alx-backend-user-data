2245d5990be7626591cfad307aa2fcbefc552fe6
Emeka Emodi <emodiemeka@gmail.com>
2023-11-08 23:23:34 +0000

    feat: Implemented request filtering and API authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,39 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Initialize the 'auth' variable based on the env variable AUTH_TYPE
+auth = None
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+# Define a list of paths that don't need authentication
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request():
+    """Filter and authenticate incoming API requests.
+    """
+    if auth is None:
+        pass
+
+    current_path = request.path
+
+    if auth.require_auth(current_path, excluded_paths):
+        authorization_header = auth.authorization_header(request)
+        current_user = auth.current_user(request)
+
+        if authorization_header is None:
+            abort(401, description="Unauthorized")
+
+        if current_user is None:
+            abort(403, description="Forbidden")
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
