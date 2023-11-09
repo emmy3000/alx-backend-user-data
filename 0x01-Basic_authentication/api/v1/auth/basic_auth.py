981de052d1822171cbf00773242e75cb46f12d50
Emeka Emodi <emodiemeka@gmail.com>
2023-11-09 00:52:59 +0000

    feat: Implement decode_base64_authorization_header method in BasicAuth class for Base64 decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Custom basic API authentication
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -31,3 +33,28 @@ class BasicAuth(Auth):
             return None
 
         return auth_parts[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """"Decode a Base64 authorization header.
+
+        Args:
+          base64_authorization_header (str): The Base64 authorization header
+          to decode.
+
+        Return:
+          str: The decoded value as a UTF-8 string.
+
+        If the input is None or not a valid string or not valid Base64,
+        it returns None.
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except binascii.Error:
+            return None
