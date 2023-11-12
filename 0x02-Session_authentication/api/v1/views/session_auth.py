bb74b22214fddf4dec2a84c3cb23cc392d771a2a
Emeka Emodi <emodiemeka@gmail.com>
2023-11-12 04:36:14 +0000

    Implement authentication routes for user login and logout in the Users view module

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Users view module
+"""
+import os
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session():
+    """Method responsible for handling user's login.
+
+    Return:
+        jsonify: Dictionary representation of the user if found,
+        else an error message.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+        for user in users:
+            if user.is_valid_password(password):
+                from api.v1.app import auth
+                session_id = auth.create_session(user.id)
+                response = jsonify(user.to_json())
+                session_name = os.getenv('SESSION_NAME')
+                response.set_cookie(session_name, session_id)
+                return response
+
+    return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def handle_logout():
+    """Method responsible for handling user's logout.
+
+    Returns:
+      jsonify: Empty response with HTTP status code 200
+      on successful logout.
+              Aborts with HTTP status code 404 if logout fails.
+    """
+    from api.v1.app import auth
+
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+
+    abort(404)
